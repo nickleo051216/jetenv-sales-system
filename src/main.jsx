b53881323d9dbaaedb3905d30f5c0ba975d0695d
Nickjet <jetenv02@gmail.com>
2025-12-11 16:38:16 +0800

    Fix: 確認四個檔案內容正確

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css' // ⚠️ 最重要的一行：引入上面的 index.css
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
